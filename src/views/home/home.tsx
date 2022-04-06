5c93d73fd6d45c947ddd2b4781f04a7bee3e6731
eudemonia <173537077@qq.com>
2022-04-06 18:02:18 +0800

    修改

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { APP_TEST } from '../../router'
 import { useDispatch, useSelector } from 'react-redux'
 import { getUserInfo } from '@src/controller/reducer/globalSlice'
+import { Button } from 'antd'
 import './home.module.less'
 
 const Home = (props: any): JSX.Element => {
@@ -26,6 +27,7 @@ const Home = (props: any): JSX.Element => {
         <span>Home</span>
       </Link>
       <div className='test1'>热更新似乎a不是s111很好用asd啊{test}</div>
+      <Button type='primary'>123</Button>
     </div>
   )
 }
