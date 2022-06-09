c2e3e657a113ce93790fbec1cf9ed2f70ca432da
eudemonia <173537077@qq.com>
2022-06-09 18:54:15 +0800

    remove animation 不稳定的效果

--- src/views/container/AutoMLLayout/AutoMLLayout.tsx
+++ src/views/container/AutoMLLayout/AutoMLLayout.tsx
@@ -1,19 +1,15 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import NavLinkBar from './NavLinkBar'
-import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 import './AutoMLLayout.module.less'
 
 const AutoMLLayout = (): JSX.Element => {
   const location = useLocation()
+  console.log(location.pathname, 'location.pathname')
   return (
     <div styleName='AutoMLLayout'>
       <NavLinkBar/>
-      <TransitionGroup className="router-wrapper">
-        <CSSTransition key={location.pathname} timeout={500} classNames="fade">
-          <Outlet />
-        </CSSTransition>
-      </TransitionGroup>
+      <Outlet />
     </div>
   )
 }
