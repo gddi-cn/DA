f4be348e44cce194457c9e18aec6684488fa21ae
eudemonia <173537077@qq.com>
2022-06-27 16:06:51 +0800

    delete comment

--- src/controller/reducer/index.ts
+++ src/controller/reducer/index.ts
@@ -1,21 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import globalSlice from './globalSlice'
 import tasksSilce from './tasksSilce'
-// const middleware = [thunk, logger];
-// const test = (state: any, ac: any) => {
-//   console.log(state)
-//   console.log(ac)
-//   return state || {}
-// }
+
 const rootReducer = combineReducers({
   globalSlice,
   tasksSilce
   // test
 });
 export type RootState = ReturnType<typeof rootReducer>
-// const store = createStore(
-//   reducers,
-//   applyMiddleware(...middleware)
-// );
 
 export default rootReducer
