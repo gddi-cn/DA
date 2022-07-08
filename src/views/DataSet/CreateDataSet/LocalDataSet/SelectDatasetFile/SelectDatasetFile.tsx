295a7ee28b52b12fbc717335322b0f10954e535e
eudemonia <173537077@qq.com>
2022-07-08 20:19:29 +0800

    commit and go home

--- src/views/DataSet/CreateDataSet/LocalDataSet/SelectDatasetFile/SelectDatasetFile.tsx
+++ src/views/DataSet/CreateDataSet/LocalDataSet/SelectDatasetFile/SelectDatasetFile.tsx
@@ -1,11 +1,15 @@
 
+import { FooterBar } from '@src/UIComponents'
 import './SelectDatasetFile.module.less'
 
 const SelectDatasetFile = (props: any): JSX.Element => {
   console.log(props)
   return (
     <div styleName='SelectDatasetFile'>
-          SelectDatasetFile
+      <div className='SelectDatasetFile_wrap'>
+            123
+      </div>
+      <FooterBar rightContent={null} />
     </div>
   )
 }
