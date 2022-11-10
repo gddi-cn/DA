99eeb4be5b5b27598283187c2a55278486f89420
amaneru <amaneru@163.com>
2022-11-10 14:22:52 +0800

    fix: footerbar position

--- src/UIComponents/FooterBar/FooterBar.tsx
+++ src/UIComponents/FooterBar/FooterBar.tsx
@@ -1,19 +1,16 @@
 import './FooterBar.module.less'
 
 type Props = {
-    rightContent:React.ReactNode
+  rightContent: React.ReactNode
 }
 
 const FooterBar = (props: Props): JSX.Element => {
   const { rightContent } = props
   return (
     <div styleName='FooterBar'>
-      <div className="g_footer_padding" />
-      <div className="g_footer_container">
-        <div />
-        <div className='g_footer_right_wrap'>
-          {rightContent}
-        </div>
+      <div></div>
+      <div className='right_wrap'>
+        {rightContent}
       </div>
     </div>
   )
