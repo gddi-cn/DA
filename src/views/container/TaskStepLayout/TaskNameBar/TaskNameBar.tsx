eb70fee77fff046a110aeae34c322ab6fd1ff289
amaneru <amaneru@163.com>
2023-02-27 11:16:02 +0800

    feat: delete current task

--- src/views/container/TaskStepLayout/TaskNameBar/TaskNameBar.tsx
+++ src/views/container/TaskStepLayout/TaskNameBar/TaskNameBar.tsx
@@ -5,15 +5,30 @@ import { Modal, Input, message } from 'antd'
 import { ChangeEvent, useRef, useState } from 'react'
 import { isNil } from 'lodash'
 import { modifyActiveTask } from '@reducer/tasksSilce'
+import styled from 'styled-components'
+import {ExclamationCircleOutlined} from "@ant-design/icons";
 
 import './TaskNameBar.module.less'
-import styled from 'styled-components'
 import { socketPushMsgForProject } from '@src/views/ghooks'
+import { PrimaryBtn, SecondaryBtn } from '@src/components/Button'
+import { useHeader } from '@src/hooks/header'
 
 const EditICon = styled(EditOutlined)`
   path {
     fill: #62B0E5;
   }
+`
+
+const Footer = styled.div`
+  display: felx;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const FooterRight = styled.div`
+  display: felx;
+  align-items: center;
+  column-gap: 10px;
 `
 
 const TaskNameBar = (): JSX.Element => {
@@ -26,7 +41,10 @@ const TaskNameBar = (): JSX.Element => {
   const dispatch = useDispatch()
   const [isModalVisible, setIsModalVisible] = useState(false)
   const [loading, setLoading] = useState(false)
+  const [deleting, setDeleting] = useState<boolean>(false)
   const taskName = useRef<undefined|string|null>(undefined)
+
+  const { deleteCurrentProject } = useHeader()
   
   const activePipeLine = useSelector((state: RootState) => {
     return state.tasksSilce.activePipeLine
@@ -65,18 +83,72 @@ const TaskNameBar = (): JSX.Element => {
   };
 
   const handleCancel = () => {
+    if (loading) return
     setIsModalVisible(false);
   };
 
   const handleOnchange = (v: ChangeEvent<HTMLInputElement>) => {
     taskName.current = v.target.value
   }
+
+  const handleDelete = () => {
+    if (loading || deleting) return
+
+    setIsModalVisible(false)
+
+    Modal.confirm({
+      title: '删除任务',
+      icon: <ExclamationCircleOutlined />,
+      content: '删除后将无法恢复，是否确定删除？',
+      okType: 'danger',
+      okText: '确定',
+      cancelText: '取消',
+      onCancel: () => setIsModalVisible(true),
+      onOk: async () => {
+        setDeleting(true)
+
+        const success = await deleteCurrentProject()
+
+        setDeleting(false)
+
+        if (!success) return
+
+        message.success('删除成功')
+      },
+    });
+  }
+
   return (
     <div styleName='TaskNameBar'>
       <div className='edit_icon_wrap' >
         <EditICon onClick={handleModifyTaskName} />
-        <Modal destroyOnClose title="修改训练基础信息" open={isModalVisible} onOk={handleOk} onCancel={handleCancel} closable={false} confirmLoading={loading}>
-
+        <Modal
+          destroyOnClose
+          title="修改训练基础信息"
+          open={isModalVisible}
+          onCancel={handleCancel}
+          closable={false}
+          footer={
+            <Footer>
+              <SecondaryBtn
+                color='error'
+                onClick={handleDelete}
+                disabled={loading}
+                loading={deleting}
+              >
+                删除
+              </SecondaryBtn>
+              <FooterRight>
+                <SecondaryBtn onClick={handleCancel} disabled={loading}>
+                  取消
+                </SecondaryBtn>
+                <PrimaryBtn onClick={handleOk} loading={loading}>
+                  确定
+                </PrimaryBtn>
+              </FooterRight>
+            </Footer>
+          }
+        >
           <Input maxLength={20} onChange={handleOnchange} defaultValue={task_name} />
           <p className='ant_moddl_tips'>最多20个字符</p>
         </Modal>
