34cbb5d909846445fc17094ea6a1607c189e4e5f
amaneru <amaneru@163.com>
2023-11-14 18:29:57 +0800

    - feat: hide auto labeling

--- src/views/Labeling/Labeling.tsx
+++ src/views/Labeling/Labeling.tsx
@@ -1,143 +1,156 @@
+import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
-import http from "@src/utils/http";
-import {Box, CircularProgress, Typography} from "@mui/material";
-import userAPI from "@src/apis/user";
-import {useCreateAndCheckoutTask} from "@src/hooks/task";
-import {message} from "antd";
-
-const getUrl = async () => {
-  try {
-    const { data }: { data: string | undefined } = await http.get('/v3/autolabel/addr')
-    return {
-      success: true,
-      data,
-    }
-  } catch (e) {
-    console.error(e)
-    return {
-      success: false,
-    }
-  }
-}
+// import http from "@src/utils/http";
+// import {Box, CircularProgress, Typography} from "@mui/material";
+// import userAPI from "@src/apis/user";
+// import {useCreateAndCheckoutTask} from "@src/hooks/task";
+// import {message} from "antd";
+//
+// const getUrl = async () => {
+//   try {
+//     const { data }: { data: string | undefined } = await http.get('/v3/autolabel/addr')
+//     return {
+//       success: true,
+//       data,
+//     }
+//   } catch (e) {
+//     console.error(e)
+//     return {
+//       success: false,
+//     }
+//   }
+// }
+//
+// const Labeling: React.FC = () => {
+//   const iframeRef = React.useRef<HTMLIFrameElement>(null)
+//   const [loading, setLoading] = React.useState(true)
+//   const [url, setUrl] = React.useState<string | null>('')
+//   const createAndCheckoutTask = useCreateAndCheckoutTask()
+//
+//   const handleTrain = async (datasetId: string) => {
+//     await createAndCheckoutTask(datasetId)
+//   }
+//
+//   const handleRefreshToke = async () => {
+//     await userAPI.info()
+//     // const { token } = data
+//     // localStorage.setItem('token', token)
+//     const $f = iframeRef.current
+//     if (!$f?.contentWindow) return
+//     $f.contentWindow.postMessage({
+//       type: 'token',
+//       payload: window.localStorage.getItem('token'),
+//     }, '*')
+//   }
+//
+//   React.useEffect(
+//     () => {
+//       setLoading(true)
+//       iframeRef.current?.addEventListener('open', () => {
+//         console.log('load')
+//       })
+//       getUrl()
+//         .then(({ success, data }) => {
+//           if (!success || !data) {
+//             setUrl(null)
+//             return
+//           }
+//           setUrl(data)
+//         })
+//         .finally(() => {
+//           setLoading(false)
+//         })
+//     },
+//     []
+//   )
+//
+//   // event binding
+//   React.useEffect(
+//     () => {
+//       const handleMessage = (e: MessageEvent) => {
+//         const { type, payload }: { type: string, payload: unknown } = e.data
+//
+//         switch (type) {
+//           case 'train':
+//             handleTrain((payload as { datasetId: string }).datasetId).catch(console.error)
+//             break
+//           case 'network':
+//             if ((payload as { code: number }).code === 401) {
+//               handleRefreshToke().catch(console.error)
+//             } else {
+//               const { msg } = payload as { code: number, msg: string }
+//               message.error(msg)
+//             }
+//             break
+//           default:
+//             break
+//         }
+//       }
+//
+//       window.addEventListener('message', handleMessage)
+//
+//       return () => {
+//         window.removeEventListener('message', handleMessage)
+//       }
+//     },
+//     []
+//   )
+//
+//   return (
+//     <div
+//       style={{
+//         height: 'calc(100vh - 50px)',
+//         lineHeight: 1,
+//         fontSize: 0,
+//         margin: '0 auto',
+//       }}
+//     >
+//       {
+//         loading ? (
+//           <Box sx={{ height: '100%', display: 'grid', placeItems: 'center' }}>
+//             <CircularProgress />
+//           </Box>
+//         ) : (
+//           url === null ? (
+//             <Box sx={{ height: '100%', display: 'grid', placeItems: 'center' }}>
+//               <Typography color={'text.secondary'}>系统繁忙，请稍后再试</Typography>
+//             </Box>
+//           ) : (
+//             <iframe
+//               ref={iframeRef}
+//               onLoad={(e) => {
+//                 (e.target as HTMLIFrameElement).contentWindow?.postMessage({
+//                   type: 'initToken',
+//                   payload: window.localStorage.getItem('token'),
+//                 }, '*')
+//               }}
+//               autoFocus
+//                // src={ 'http://localhost:8080/'}
+//               src={url}
+//               style={{
+//                 width: '1px',
+//                 minWidth: '100%',
+//                 height: '100%',
+//                 border: 'none'
+//               }}
+//               allowFullScreen
+//             />
+//           )
+//         )
+//       }
+//     </div>
+//   )
+// }
+//
 
 const Labeling: React.FC = () => {
-  const iframeRef = React.useRef<HTMLIFrameElement>(null)
-  const [loading, setLoading] = React.useState(true)
-  const [url, setUrl] = React.useState<string | null>('')
-  const createAndCheckoutTask = useCreateAndCheckoutTask()
-
-  const handleTrain = async (datasetId: string) => {
-    await createAndCheckoutTask(datasetId)
-  }
-
-  const handleRefreshToke = async () => {
-    await userAPI.info()
-    // const { token } = data
-    // localStorage.setItem('token', token)
-    const $f = iframeRef.current
-    if (!$f?.contentWindow) return
-    $f.contentWindow.postMessage({
-      type: 'token',
-      payload: window.localStorage.getItem('token'),
-    }, '*')
-  }
-
-  React.useEffect(
-    () => {
-      setLoading(true)
-      iframeRef.current?.addEventListener('open', () => {
-        console.log('load')
-      })
-      getUrl()
-        .then(({ success, data }) => {
-          if (!success || !data) {
-            setUrl(null)
-            return
-          }
-          setUrl(data)
-        })
-        .finally(() => {
-          setLoading(false)
-        })
-    },
-    []
-  )
-
-  // event binding
-  React.useEffect(
-    () => {
-      const handleMessage = (e: MessageEvent) => {
-        const { type, payload }: { type: string, payload: unknown } = e.data
-
-        switch (type) {
-          case 'train':
-            handleTrain((payload as { datasetId: string }).datasetId).catch(console.error)
-            break
-          case 'network':
-            if ((payload as { code: number }).code === 401) {
-              handleRefreshToke().catch(console.error)
-            } else {
-              const { msg } = payload as { code: number, msg: string }
-              message.error(msg)
-            }
-            break
-          default:
-            break
-        }
-      }
-
-      window.addEventListener('message', handleMessage)
-
-      return () => {
-        window.removeEventListener('message', handleMessage)
-      }
-    },
-    []
-  )
-
   return (
-    <div
-      style={{
-        height: 'calc(100vh - 50px)',
-        lineHeight: 1,
-        fontSize: 0,
-        margin: '0 auto',
-      }}
-    >
-      {
-        loading ? (
-          <Box sx={{ height: '100%', display: 'grid', placeItems: 'center' }}>
-            <CircularProgress />
-          </Box>
-        ) : (
-          url === null ? (
-            <Box sx={{ height: '100%', display: 'grid', placeItems: 'center' }}>
-              <Typography color={'text.secondary'}>系统繁忙，请稍后再试</Typography>
-            </Box>
-          ) : (
-            <iframe
-              ref={iframeRef}
-              onLoad={(e) => {
-                (e.target as HTMLIFrameElement).contentWindow?.postMessage({
-                  type: 'initToken',
-                  payload: window.localStorage.getItem('token'),
-                }, '*')
-              }}
-              autoFocus
-               // src={ 'http://localhost:8080/'}
-              src={url}
-              style={{
-                width: '1px',
-                minWidth: '100%',
-                height: '100%',
-                border: 'none'
-              }}
-              allowFullScreen
-            />
-          )
-        )
-      }
-    </div>
+    <Box style={{ height: 'calc(100vh - 50px)', background: 'rgb(241, 245, 249)', display: 'grid', placeItems: 'center' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', rowGap: 16 }}>
+        <Typography variant='h5'>即将上线，敬请期待</Typography>
+        <Button component='a' href='/' variant='outlined'>返回首页</Button>
+      </div>
+    </Box>
   )
 }
 
