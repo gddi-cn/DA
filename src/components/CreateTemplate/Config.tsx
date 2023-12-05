406fa0f542f2ac41a7f07771d796eb31be4a9600
amaneru <amaneru@163.com>
2023-12-05 20:10:04 +0800

    - change(PLAT-231): update gddi-app-builder version to 0.2.7

--- src/components/CreateTemplate/Config.tsx
+++ src/components/CreateTemplate/Config.tsx
@@ -1,55 +1,48 @@
-import React from 'react'
-import appFlowAPI from '@src/apis/appFlow'
-import { pipelineAtom } from './store'
-import { useSetAtom } from 'jotai'
+import React from "react";
+import appFlowAPI from "@src/apis/appFlow";
+import { pipelineAtom } from "./store";
+import { useSetAtom } from "jotai";
 import AppBuilder, { ModuleDefinition } from "gddi-app-builder";
 
 const useConfig = () => {
-  const [version, setVersion] = React.useState<AppFlow.ModuleDefinition.Version['version'] | undefined>(undefined)
-  const [moduleDefinitions, setModuleDefinitions] = React.useState<ModuleDefinition | undefined>(undefined)
-  const setPipeline = useSetAtom(pipelineAtom)
+  const [version, setVersion] = React.useState<
+    AppFlow.ModuleDefinition.Version["version"] | undefined
+  >(undefined);
+  const [moduleDefinitions, setModuleDefinitions] = React.useState<
+    ModuleDefinition | undefined
+  >(undefined);
+  const setPipeline = useSetAtom(pipelineAtom);
 
+  React.useLayoutEffect(() => {
+    appFlowAPI.getDefaultConfig().then(({ success, data }) => {
+      if (!success || !data) return;
+      const { moduleDefinitions, version } = data;
+      setVersion(version || "");
+      setModuleDefinitions(moduleDefinitions || {});
+    });
 
-  React.useLayoutEffect(
-    () => {
-      appFlowAPI
-        .getDefaultConfig()
-        .then(({ success, data }) => {
-          if (!success || !data) return
-          const { moduleDefinitions, version } = data
-          setVersion(version || '')
-          setModuleDefinitions(moduleDefinitions || {})
-        })
-
-      return () => {
-        setVersion(undefined)
-        setModuleDefinitions(undefined)
-        setPipeline(undefined)
-      }
-    },
-    []
-  )
+    return () => {
+      setVersion(undefined);
+      setModuleDefinitions(undefined);
+      setPipeline(undefined);
+    };
+  }, []);
 
   return {
     version,
     moduleDefinitions,
     handleChange: setPipeline,
-  }
-}
+  };
+};
 
 const Config: React.FC = () => {
-  const {
-    version,
-    moduleDefinitions,
-    handleChange,
-  } = useConfig()
-  console.log({ moduleDefinitions, version })
+  const { version, moduleDefinitions, handleChange } = useConfig();
 
-  if (!moduleDefinitions) return null
+  if (!moduleDefinitions) return null;
 
   return (
     <AppBuilder
-      version={'v3'}
+      version={"v3"}
       modules={moduleDefinitions || {}}
       onFlowChange={(val: any) => handleChange(val)}
       propsDisabled
@@ -64,8 +57,7 @@ const Config: React.FC = () => {
     //   version={version || 'v1'}
     //   layoutVertically
     // />
-  )
-}
+  );
+};
 
-export default Config
-
+export default Config;
